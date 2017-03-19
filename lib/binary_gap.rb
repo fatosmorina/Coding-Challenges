eff237bb520844b3d8420df25062372dae2f29ed
Fatos Morina <fatosi.morina@gmail.com>
2017-03-19 15:59:23 +0100

    Solve BinaryGap

---
+++ lib/binary_gap.rb
@@ -0,0 +1,14 @@
+#https://codility.com/programmers/lessons/1-iterations/binary_gap/
+
+class BinaryGap
+  def solution(n)
+    binary = n.to_s(2)
+    parts = binary.split('1')
+    parts.pop if n % 2 == 0
+    if parts.empty?
+      0
+    else
+      parts.map {|part| part.length}.max
+    end
+  end
+end
---
+++ spec/binary_gap_spec.rb
@@ -0,0 +1,19 @@
+require '../lib/binary_gap'
+
+describe BinaryGap do
+
+  it 'should return 2 for the input 9' do
+    expect(subject.solution(9)).to eq(2)
+  end
+
+  it 'should return 20 for the input 1' do
+    expect(subject.solution(20)).to eq(1)
+  end
+
+  it 'should return 15 for the input 0' do
+    expect(subject.solution(15)).to eq(0)
+  end
+
+
+
+end
