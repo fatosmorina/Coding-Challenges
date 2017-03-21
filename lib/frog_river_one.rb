c2db70ed5a1ea1985e9cdfb63fb05c6e75b6ae2e
Fatos Morina <fatosi.morina@gmail.com>
2017-03-21 10:31:27 +0100

    Remove unused method

--- lib/frog_river_one.rb
+++ lib/frog_river_one.rb
@@ -14,20 +14,4 @@ class FrogRiverOne
     -1
   end
 
-  def solution(a, b, m)
-    sum_a = a.reduce(0, :+)
-    sum_b = b.reduce(0, :+)
-    d = sum_b - sum_a
-    if d % 2 == 1
-      return false
-    end
-    d = d / 2
-    for i in 1..n do
-      if 0 <= b[i] - d && b[i] - d <= 0 && a[b[i] - d] > 0
-        return true
-      end
-    end
-  end
-
-
 end
