7f6fd96603796171c199de256ff02f18cf1a2493
Fatos Morina <fatosi.morina@gmail.com>
2017-03-27 08:00:36 +0200

    Add second solution of finding distinct elements in an array

--- lib/distinct.rb
+++ lib/distinct.rb
@@ -8,4 +8,16 @@ class Distinct
     set = Set.new(array)
     set.size
   end
+
+  def second_solution(array)
+    array.sort!
+    distinct = []
+    1.upto(array.length) do |index|
+      if array[index-1] != array[index]
+        distinct<<array[index-1]
+      end
+    end
+    distinct.size
+  end
+
 end
--- spec/distinct_spec.rb
+++ spec/distinct_spec.rb
@@ -1,7 +1,19 @@
 require '../lib/distinct.rb'
 
 describe Distinct do
-  it 'should return the number of distinct elements equal to 5' do
+  it 'should return the number of distinct elements equal to 5 for the first solution' do
     expect(subject.solution([1,1,4,2,3,5,5,3,1])).to eq(5)
+  end
+
+  it 'should return the number of distinct elements equal to 3 for the first solution' do
+    expect(subject.solution([1,1,2,35,2,2,35])).to eq(3)
+  end
+
+  it 'should return the number of distinct elements equal to 5 for the second solution' do
+    expect(subject.second_solution([1,1,4,2,3,5,5,3,1])).to eq(5)
+  end
+
+  it 'should return the number of distinct elements equal to 3 for the second solution' do
+    expect(subject.second_solution([1,1,2,35,2,2,35])).to eq(3)
   end
 end
