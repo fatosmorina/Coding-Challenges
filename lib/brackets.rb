a83b6124169533349e6e8636a6ded0f33901d569
Fatos Morina <fatosi.morina@gmail.com>
2017-03-30 08:32:27 +0200

    Solve Brackets

--- lib/brackets.rb
+++ lib/brackets.rb
@@ -1,73 +1,24 @@
 class Brackets
 
-  def solution(string)
-    array = []
-    0.upto(string.length-1) do |index|
-      letter = string[index]
-      if letter == '(' || letter == '[' || letter == '{'
-        array.push(1)
+  def solution(s)
+    pairs = {
+        '{' => '}',
+        '[' => ']',
+        '(' => ')'
+    }
+
+    stack = []
+
+    s.chars.each do |char|
+      if pairs.keys.include?(char)
+        stack << char
       else
-        if array.length
-          first = array.pop
-          if letter ==')' && first == '(' || letter ==']' && first == '['  || letter =='}' && first == '{'
-            continue
-          else
-            return 0
-          else 0
-        end
-        end
-        return array.length > 0 ? 0 : 1
-    end
-    k = 0
-    0.upto(string.length-1) do |index|
-      if string[index] == '('
-        array << string[index]
-      else
-        if array.length
-          array.pop
-        end
-        k += 1
+        return 0 if pairs[stack.pop] != char
       end
     end
-    k
+
+    return 0 if stack.any?
+    1
   end
-
-  def solution2(array)
-    num = 0
-    0.upto(array.length) do |index|
-      num += array[index].to_i * (-2**index)
-    end
-    num = -num
-    ret_val = []
-    while num != 0
-      if num == -1
-        ret_val << 1
-        ret_val << 1
-        break
-      elsif num == 1
-        ret_val.push(1)
-        break
-      end
-      remainder = num % (-2)
-      remainder = remainder == 0 ? 0 : remainder
-      num = float2int(num / (-2))
-      if remainder < 0
-        num += 1
-        remainder += 2
-      end
-      ret_val.push(remainder)
-    end
-    ret_val
-  end
-
-  private
-
-  def float2int(value)
-    value || 0
-  end
-
 
 end
-
-
-puts Brackets.new.solution2([1,0,0,1,1])
