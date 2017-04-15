92f991f414e3600b216d6245011ab708688d581d
Fatos Morina <fatosi.morina@gmail.com>
2017-04-15 19:32:10 +0200

    Add problem description at AnagramWord

--- lib/codewars/break_camel_case.rb
+++ lib/codewars/break_camel_case.rb
@@ -1,3 +1,11 @@
+=begin
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+solution('camelCasing') # => should return 'camel Casing'  
+=end
+
 #http://www.codewars.com/kata/5208f99aee097e6552000148/train/ruby
 
 class BreakCamelCase
