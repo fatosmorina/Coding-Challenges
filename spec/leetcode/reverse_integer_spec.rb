84cb942840b2005e424ef4d17af21aa200860117
Fatos Morina <fatosi.morina@gmail.com>
2017-04-20 08:07:06 +0200

    Put reverse_integer method inside a class

--- spec/leetcode/reverse_integer_spec.rb
+++ spec/leetcode/reverse_integer_spec.rb
@@ -1,7 +1,23 @@
 require '../../lib/leetcode/reverse_integer'
 
-describe InverseInteger do
+describe ReverseInteger do
   it 'should return 0 when the reversed is larger than max integer'
     expect(subject.reverse(123456789)).to eq(0)
   end
+end
+
+describe BinaryGap do
+
+  it 'should return 2 for the input 9' do
+    expect(subject.solution(9)).to eq(2)
+  end
+
+  it 'should return 20 for the input 1' do
+    expect(subject.solution(20)).to eq(1)
+  end
+
+  it 'should return 15 for the input 0' do
+    expect(subject.solution(15)).to eq(0)
+  end
+
 end
