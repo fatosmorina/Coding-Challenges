9a244e5edf1d193f1fda75bc908457c26b2cb26f
Fatos Morina <fatosi.morina@gmail.com>
2017-04-20 07:53:00 +0200

    Solve reverse_integer

---
+++ lib/leetcode/reverse_integer.rb
@@ -0,0 +1,17 @@
+
+#https://leetcode.com/problems/reverse-integer/#/description
+def reverse(integer)
+  input = integer.abs
+  result = 0
+  if integer < 0
+    result = "-#{input.to_s.reverse!}".to_i
+  else
+    result = "#{input.to_s.reverse!}".to_i
+  end
+  if result.abs > 2147483647
+    0
+  else
+    result
+  end
+end
+puts reverse(1534236469)
