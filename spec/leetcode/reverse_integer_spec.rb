2608aafabdfa0a09eac6cae7b62134ebcc9a730c
Fatos Morina <fatosi.morina@gmail.com>
2017-04-20 08:11:28 +0200

    Add missing do at reverse_integer spec

--- spec/leetcode/reverse_integer_spec.rb
+++ spec/leetcode/reverse_integer_spec.rb
@@ -1,23 +1,7 @@
 require '../../lib/leetcode/reverse_integer'
 
 describe ReverseInteger do
-  it 'should return 0 when the reversed is larger than max integer'
+  it 'should return 0 when the reversed is larger than max integer' do
     expect(subject.reverse(123456789)).to eq(0)
   end
-end
-
-describe BinaryGap do
-
-  it 'should return 2 for the input 9' do
-    expect(subject.solution(9)).to eq(2)
-  end
-
-  it 'should return 20 for the input 1' do
-    expect(subject.solution(20)).to eq(1)
-  end
-
-  it 'should return 15 for the input 0' do
-    expect(subject.solution(15)).to eq(0)
-  end
-
 end
