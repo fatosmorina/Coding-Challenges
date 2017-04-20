e9f667cf9ac729153ce7b7cd0ec5a1785a90d8ca
Fatos Morina <fatosi.morina@gmail.com>
2017-04-20 08:16:01 +0200

    It should reverse_integer

--- spec/leetcode/reverse_integer_spec.rb
+++ spec/leetcode/reverse_integer_spec.rb
@@ -8,4 +8,8 @@ describe ReverseInteger do
   it 'should return -123' do
     expect(subject.reverse(-321)).to eq(-123)
   end
+
+  it 'should return 1234567' do
+    expect(subject.reverse(1234567)).to eq(7654321)
+  end
 end
