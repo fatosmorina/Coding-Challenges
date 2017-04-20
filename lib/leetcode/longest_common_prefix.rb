f7573b8a06e51f54442fa1e8dab4778b89adc5c1
Fatos Morina <fatosi.morina@gmail.com>
2017-04-20 10:37:26 +0200

    Solve LongestCommonPrefix

--- lib/leetcode/longest_common_prefix.rb
+++ lib/leetcode/longest_common_prefix.rb
@@ -1,10 +1,19 @@
 
 #https://leetcode.com/problems/longest-common-prefix/#/description
-
 class LongestCommonPrefix
+  def longest_common_prefix(strs)
+    if strs.nil? || strs.empty?
+      return ''
+    end
+    prefix = strs.min { |x, y| x.size <=> y.size }
 
-  def longest_common_practice(strs)
-
+    strs.each do |i|
+      while i.index(prefix) != 0
+          prefix = prefix[0...prefix.length-1]
+        end
+      end
+    prefix
   end
 
 end
+
