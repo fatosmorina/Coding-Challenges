afc09029e04a3596bd962f1b6fb9861973567696
Fatos Morina <fatosi.morina@gmail.com>
2017-04-21 08:11:48 +0200

    Add spec for PrimeNumber

---
+++ lib/leetcode/count_primes.rb
@@ -0,0 +1,9 @@
+
+#https://leetcode.com/problems/count-primes/#/description
+
+class CountPrimes
+
+  def solution(number)
+
+  end
+end
---
+++ spec/leetcode/count_primes_spec.rb
@@ -0,0 +1,9 @@
+require '../../lib/leetcode/count_primes'
+
+describe CountPrimes do
+
+  it 'should return 3 for 9' do
+    expect(subject.solution(9)).to eq(3)
+  end
+
+end
