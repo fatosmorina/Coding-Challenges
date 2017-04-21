b98746aae619ed1867b90581d6fad82d5c03838f
Fatos Morina <fatosi.morina@gmail.com>
2017-04-21 08:20:22 +0200

    Add additional specs

--- spec/leetcode/count_primes_spec.rb
+++ spec/leetcode/count_primes_spec.rb
@@ -3,7 +3,17 @@ require '../../lib/leetcode/count_primes'
 describe CountPrimes do
 
   it 'should return 3 for 9' do
-    expect(subject.solution(9)).to eq(3)
+    expect(subject.count_primes(9)).to eq(3)
   end
+
+
+  it 'should return 0 for 0' do
+    expect(subject.count_primes(0)).to eq(0)
+  end
+
+  it 'should return 0 for 1' do
+    expect(subject.count_primes(1)).to eq(0)
+  end
+
 
 end
