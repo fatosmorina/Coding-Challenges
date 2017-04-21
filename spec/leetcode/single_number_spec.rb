db52b3957779bc01f8d854e2e0aaaf81cc60eeea
Fatos Morina <fatosi.morina@gmail.com>
2017-04-21 07:47:06 +0200

    Add additional spec to SingleNumber

--- spec/leetcode/single_number_spec.rb
+++ spec/leetcode/single_number_spec.rb
@@ -6,4 +6,8 @@ describe SingleNumber do
     expect(subject.single_number([1,2,3,3,2])).to eq(1)
   end
 
+  it 'should return -3 for the given array [-1, -1, -2, 3, -3, 3, -2]' do
+    expect(subject.single_number([-1, -1, -2, 3, -3, 3, -2])).to eq(-3)
+  end
+
 end
