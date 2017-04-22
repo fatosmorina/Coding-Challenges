90cefa93db34923869b5e4bbf173894214b99b15
Fatos Morina <fatosi.morina@gmail.com>
2017-04-22 07:35:03 +0200

    Solve CountPrimes

--- lib/leetcode/count_primes.rb
+++ lib/leetcode/count_primes.rb
@@ -1,9 +1,19 @@
 
 #https://leetcode.com/problems/count-primes/#/description
 
+
 class CountPrimes
 
-  def solution(number)
-
+  def count_primes(n)
+    mark = [false] * (n + 1)
+    counter = 0
+    2.upto(n) do |i|
+      next if mark[i] == true
+      counter += 1
+      (i * i).step n, i do |j| mark[j] = true end
+    end
+    counter
   end
+
+
 end
--- spec/leetcode/count_primes_spec.rb
+++ spec/leetcode/count_primes_spec.rb
@@ -2,17 +2,16 @@ require '../../lib/leetcode/count_primes'
 
 describe CountPrimes do
 
-  it 'should return 3 for 9' do
-    expect(subject.count_primes(9)).to eq(3)
-  end
-
-
   it 'should return 0 for 0' do
     expect(subject.count_primes(0)).to eq(0)
   end
 
   it 'should return 0 for 1' do
     expect(subject.count_primes(1)).to eq(0)
+  end
+
+  it 'should return 4 for 2' do
+    expect(subject.count_primes(4)).to eq(2)
   end
 
 
