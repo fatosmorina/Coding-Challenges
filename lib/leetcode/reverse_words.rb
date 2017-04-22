a4dfafab4a4ff4bde3033aed7f1cf9cc849047b1
Fatos Morina <fatosi.morina@gmail.com>
2017-04-22 10:43:28 +0200

    Add a spec testing reverse Words in a string

--- lib/leetcode/reverse_words.rb
+++ lib/leetcode/reverse_words.rb
@@ -1,9 +1,8 @@
 
 #https://leetcode.com/problems/reverse-words-in-a-string-iii/#/description
 
-
+class ReverseWords
   def reverse_words(array)
     array.split(" ").map{|word| word.reverse}.join(" ")
   end
-
-p reverse_words('how are you')
+end
--- spec/leetcode/reverse_words_spec.rb
+++ spec/leetcode/reverse_words_spec.rb
@@ -1,5 +1,10 @@
 
 require '../../lib/leetcode/reverse_words'
 
-class ReverseWordsSpec
+describe ReverseWords do
+
+  it 'should reverse the words' do
+    expect(subject.reverse_words('how are you')).to eq('woh era uoy')
+  end
+
 end
