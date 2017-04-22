a7f7660c30862f9445b4596017db28062ffa9f6d
Fatos Morina <fatosi.morina@gmail.com>
2017-04-22 07:44:58 +0200

    Add spec testing for empty arrays

--- spec/leetcode/contains_duplicate_spec.rb
+++ spec/leetcode/contains_duplicate_spec.rb
@@ -8,7 +8,11 @@ describe ContainsDuplicate do
   end
 
   it 'should return true when it does not contain duplicates' do
-    expect(subject.contains_duplicate([1,2,3,4]))
+    expect(subject.contains_duplicate([1,2,3,4])).to eq(false)
+  end
+
+  it 'should return true when it does not contain any element' do
+    expect(subject.contains_duplicate([])).to eq(true)
   end
 
 end
