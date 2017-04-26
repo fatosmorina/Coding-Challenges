bea3bcf1dee57ac6b4bd5521d4c1e9eb011b4fa7
Fatos Morina <fatosi.morina@gmail.com>
2017-04-26 16:06:54 +0200

    Add test case for FindFactors

--- spec/codewars/find_factors_spec.rb
+++ spec/codewars/find_factors_spec.rb
@@ -5,7 +5,10 @@ describe FindFactors do
    it 'should return empty array when limit is greater than integer' do
      subject(subject.factors(1, 3)).to eq([])
    end
-    
 
-   
+    it 'should return [2, 3, 5, 6, 10, 15, 30] for integer = 30, limit = 2' do
+     subject(subject.factors(1, 3)).to eq([])
+   end
+
+       
 end
