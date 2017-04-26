4d5df47039953a1ef320e2b9b156dfa3bdd9716b
Fatos Morina <fatosi.morina@gmail.com>
2017-04-26 21:21:54 +0200

    Update spec description at UniqueInWordsSpec

--- spec/codewars/unique_in_words_spec.rb
+++ spec/codewars/unique_in_words_spec.rb
@@ -2,7 +2,7 @@ require '../../lib/codewars/unique_in_words'
 
 describe UniqueInWords do
 
-  it 'should return  for' do
+  it "should return ['A','B','C','D','A','B'] for AAAABBBCCDAABBB" do
     expect(subject.unique_in_order('AAAABBBCCDAABBB')).to eq( ['A','B','C','D','A','B'])
   end
 
