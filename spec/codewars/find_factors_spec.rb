665cef04e5f392f04f98542f99d645276e59cac0
Fatos Morina <fatosi.morina@gmail.com>
2017-04-26 16:08:09 +0200

    Add test case for FindFactors

--- spec/codewars/find_factors_spec.rb
+++ spec/codewars/find_factors_spec.rb
@@ -7,8 +7,14 @@ describe FindFactors do
    end
 
     it 'should return [2, 3, 5, 6, 10, 15, 30] for integer = 30, limit = 2' do
-     subject(subject.factors(1, 3)).to eq([])
+     subject(subject.factors(30, 2)).to eq([2, 3, 5, 6, 10, 15, 30])
    end
 
-       
+     it 'should return [100] for integer = 100, limit = 75' do
+     subject(subject.factors(100, 75)).to eq([100])
+   end
+
+
+    
+
 end
