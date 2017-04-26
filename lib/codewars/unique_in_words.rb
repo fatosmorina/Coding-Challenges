09336bc7cf4aa0134be8892c465dcc318675d8fa
Fatos Morina <fatosi.morina@gmail.com>
2017-04-26 20:56:19 +0200

    Solve UniqueInOrder

---
+++ lib/codewars/unique_in_words.rb
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/54e6533c92449cc251001667/train/ruby
+
+class UniqueInWords
+  
+   def unique_in_order(iterable)
+    result = [iterable[0]]
+    1.upto(iterable.length - 1) do |i|
+      if iterable[i-1] != iterable[i]
+        result<<iterable[i]
+      end
+    end
+    result
+  end
+ 
+end
---
+++ spec/codewars/unique_in_words_spec.rb
@@ -0,0 +1,10 @@
+require '../../lib/unique_in_words'
+
+describe UniqueInWords do
+
+  it 'should return  for' do
+    expect(subject.unique_in_order(AAAABBBCCDAABBB')).to eq( ['A','B','C','D','A','B'])
+  end
+
+
+end
