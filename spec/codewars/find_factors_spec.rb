5432d18ccbdfeb5cc27cf26a0a2cd9e1fbe542fd
Fatos Morina <fatosi.morina@gmail.com>
2017-04-26 16:13:04 +0200

    Add expect at specs

--- spec/codewars/find_factors_spec.rb
+++ spec/codewars/find_factors_spec.rb
@@ -3,24 +3,24 @@ require '../../lib/codewars/find_factors'
 describe FindFactors do
  
    it 'should return empty array when limit is greater than integer' do
-     subject(subject.factors(1, 3)).to eq([])
+     expect(subject.factors(1, 3)).to eq([])
    end
 
     it 'should return [2, 3, 5, 6, 10, 15, 30] for integer = 30, limit = 2' do
-     subject(subject.factors(30, 2)).to eq([2, 3, 5, 6, 10, 15, 30])
+     expect(subject.factors(30, 2)).to eq([2, 3, 5, 6, 10, 15, 30])
    end
 
     it 'should return [100] for integer = 100, limit = 75' do
-     subject(subject.factors(100, 75)).to eq([100])
+     expect(subject.factors(100, 75)).to eq([100])
    end
   
    
     it 'should return [1,5] for integer = 5, limit = 1' do
-      subject(subject.factors(5,1)).to eq([1,5])
+      expect(subject.factors(5,1)).to eq([1,5])
     end  
 
     it 'should return [5, 8, 10, 20, 40]] for integer = 40, limit = 5' do
-      subject(subject.factors(40, 5)).to eq([5, 8, 10, 20, 40])
+      expect(subject.factors(40, 5)).to eq([5, 8, 10, 20, 40])
     end  
 
   
