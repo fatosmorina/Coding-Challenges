fd38e45b2052ddefb33f1cb53b1f3c89dfb39457
Fatos Morina <fatosi.morina@gmail.com>
2017-04-27 14:59:41 +0200

    Improve require path

--- spec/codewars/divisors_spec.rb
+++ spec/codewars/divisors_spec.rb
@@ -1,6 +1,6 @@
-require '../../lib/divisors'
+require '../../lib/codewars/divisors'
 
-describe Describe do
+describe Divisors do
 
   it 'should return [2,3] for number = 6' do
     expect(subject.divisors(6)).to eq([2,3])
