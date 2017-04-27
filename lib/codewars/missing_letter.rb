cdd2223b8eaa097262dbd1cafa421a8bd3e1545d
Fatos Morina <fatosi.morina@gmail.com>
2017-04-27 09:22:55 +0200

    Solve MissingLetter

--- lib/codewars/missing_letter.rb
+++ lib/codewars/missing_letter.rb
@@ -4,7 +4,13 @@
 class MissingLetter
 
   def missing_letter(letters)
-      
+    alphabet = ('a'..'z').to_a  
+    first_letter = alphabet.index(letters[0])
+    0.upto(letters.length - 1) do |i|
+       if alphabet[i+first_letter] != letters[i]
+         return alphabet[i+first_letter] 
+       end
+    end
   end
   
 end
