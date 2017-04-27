828b6961af8f52af893838a18666f74e821f4ff5
Fatos Morina <fatosi.morina@gmail.com>
2017-04-27 09:25:14 +0200

    Rename the method as the method name in CodeWars

--- lib/codewars/missing_letter.rb
+++ lib/codewars/missing_letter.rb
@@ -3,7 +3,7 @@
 #https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/ruby
 class MissingLetter
 
-  def missing_letter(letters)
+  def find_missing_letter(letters)
     alphabet = ('a'..'z').to_a  
     first_letter = alphabet.index(letters[0])
     0.upto(letters.length - 1) do |i|
--- spec/codewars/missing_letter_spec.rb
+++ spec/codewars/missing_letter_spec.rb
@@ -3,7 +3,7 @@ require '../../lib/codewars/missing_letter.rb'
 describe MissingLetter do
 
   it "should return c for the array = ['a','b','d']" do
-    expect(subject.missing_letter(['a', 'b', 'd'])).to eq('c')
+    expect(subject.find_missing_letter(['a', 'b', 'd'])).to eq('c')
   end
   
 
