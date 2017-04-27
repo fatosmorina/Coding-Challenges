174bf5226ae8fc07b1e4ff2502ce92113b82b698
Fatos Morina <fatosi.morina@gmail.com>
2017-04-27 15:20:09 +0200

    Test DigitalRoot for number=0

--- spec/codewars/digital_root_spec.rb
+++ spec/codewars/digital_root_spec.rb
@@ -6,4 +6,10 @@ describe DigitalRoot do
     expect(subject.digital_root(15)).to eq(6)
   end
 
+  it 'should return 0 for number = 0' do
+    expect(subject.digital_root(0)).to eq(0)
+  end
+
+
+
 end
