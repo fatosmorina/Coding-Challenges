45a049e9a17c2deac05ad425811fa433d729c21b
Fatos Morina <fatosi.morina@gmail.com>
2017-04-27 15:01:22 +0200

    Add new test case for Divisors

--- spec/codewars/divisors_spec.rb
+++ spec/codewars/divisors_spec.rb
@@ -5,4 +5,12 @@ describe Divisors do
   it 'should return [2,3] for number = 6' do
     expect(subject.divisors(6)).to eq([2,3])
   end
+
+   it 'should return [11,23] for number = 253' do
+    expect(subject.divisors(253)).to eq([11,23])
+  end
+
+
+#Test.assert_equals(divisors(24), [2,3,4,6,8,12])
+
 end
