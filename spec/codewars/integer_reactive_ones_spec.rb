f6fcf632034cfe41fdad8db16e5429f4935abec8
Fatos Morina <fatosi.morina@gmail.com>
2017-04-28 12:29:07 +0200

    Add new test for integer reactive ones

--- spec/codewars/integer_reactive_ones_spec.rb
+++ spec/codewars/integer_reactive_ones_spec.rb
@@ -3,9 +3,14 @@ require '../../lib/codewars/integers_reactive_one'
 describe IntegerReactiveOnes do
   
 
-  it 'should return an array of elments' do 
+  it 'should return [[1,1], [42,2500], [246, 84100]] for m=1, n = 250' do 
     expect(subject.list_squared(1,250)).to eq([[1,1], [42,2500], [246, 84100]])
   end
 
+
+
+  it 'should return [[42,2500], [246, 84100]] for m=42, n = 250' do 
+    expect(subject.list_squared(42,250)).to eq([[42,2500], [246, 84100]])
+  end
 
 end
