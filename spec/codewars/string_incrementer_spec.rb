9db49b4c3dcc33233c19c7c9a495c3ba385dcb9a
Fatos Morina <fatosi.morina@gmail.com>
2017-04-28 11:12:46 +0200

    Rename subject.method called

--- spec/codewars/string_incrementer_spec.rb
+++ spec/codewars/string_incrementer_spec.rb
@@ -3,11 +3,11 @@ require '../../lib/codewars/string_incrementer'
 describe StringIncrementer do
 
   it 'should return foo1 for input = foo' do
-    expect(subject.string_incrementer('foo')).to eq('foo1')
+    expect(subject.increment_string('foo')).to eq('foo1')
   end
 
   it 'should return foobar002 for input = foobar001' do
-    expect(subject.string_incrementer('foobar001')).to eq('foobar002')
+    expect(subject.increment_string('foobar001')).to eq('foobar002')
   end
 
 end
