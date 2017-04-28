b485d046e5627038e1b43df39e779f02114fd125
Fatos Morina <fatosi.morina@gmail.com>
2017-04-28 11:11:53 +0200

    Solve increment_string

--- lib/codewars/string_incrementer.rb
+++ lib/codewars/string_incrementer.rb
@@ -1,15 +1,9 @@
 #https://www.codewars.com/trainer/ruby
 
 class StringIncrementer
-
-  def string_incrementer(string)
-    last_number = string[string.length - 1]
-    output = last_number.to_i+1
-    if last_number.to_i == 0
-      return "#{string}#{output}"
-    else
-      return "#{string[0...string.length-1]}#{output}"
-    end  
+  
+  def increment_string(n)
+    input.sub(/\d*$/) { |n| n.empty? ? 1 : n.succ }
   end
 
 end
