4091ce444d98ab2b9402d62512ccc9d9bbf8dbff
Fatos Morina <fatosi.morina@gmail.com>
2017-04-28 21:22:59 +0200

    Fix spec regarding anagram words

--- spec/codewars/anagram_words_spec.rb
+++ spec/codewars/anagram_words_spec.rb
@@ -2,7 +2,8 @@ require '../../lib/codewars/anagram_words'
 
 describe AnagramWords do
 
-  it 'should return labe for word able [labe, lb]' do
-    expect(subject.anagrams('label', ['labe', 'lb']))
+  it "should return 'labe' for word  ['able', lb]" do
+    expect(subject.anagrams('able', ['able', 'lb'])).to eq(['able'])
+                                                        
   end
 end
