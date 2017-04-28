a28a4fcb1c33dee2468c969b35bc037a9ba46fb4
Fatos Morina <fatosi.morina@gmail.com>
2017-04-28 11:48:19 +0200

    Solve ValidPhoneNumber

--- lib/codewars/valid_phone_number.rb
+++ lib/codewars/valid_phone_number.rb
@@ -1,23 +1,9 @@
 class ValidPhoneNumber
 
   def valid_phone_number(number)
-    
-    num = num.to_a
-    return false if num.length < 13
-    return false if num[0] != '(' || num[4] != ')' || num[5] != ' ' || num[8] != '-'
-    1.upto(num.length - 1) do |i|
-     if i != 0 && i != 4 && i != 5 && i != 8 && !is_number?(num[i])
-       return false
-     end
-    end
-    true
+    values = number.split('')
+    return true if values[0] == '(' && values[4] == ')' && values[5] == ' ' && values[9] == '-'
+    false
   end
-
-  private 
-
-  def is_number?(number)
-    Integer(number) != nil rescue false
-  end
-
 
 end
--- spec/codewars/valid_phone_number_spec.rb
+++ spec/codewars/valid_phone_number_spec.rb
@@ -2,8 +2,8 @@ require '../../lib/codewars/valid_phone_number'
 
 describe ValidPhoneNumber do
 
-  it 'should return true for (123) 456-789' do
-    expect(subject.valid_phone_number('(123) 456-789')).to be_truthy
+  it 'should return true for (123) 456-7890' do
+    expect(subject.valid_phone_number("(123) 456-7890")).to be_truthy
   end
 
 
