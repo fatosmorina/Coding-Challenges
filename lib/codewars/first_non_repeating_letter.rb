16189c2bc168b3c535525eb1d7093f6e0db33714
Fatos Morina <fatosi.morina@gmail.com>
2017-04-29 07:08:09 +0200

    Solve FirstNonRepeatingLetter

--- lib/codewars/first_non_repeating_letter.rb
+++ lib/codewars/first_non_repeating_letter.rb
@@ -6,9 +6,13 @@ class FirstNonRepeatingLetter
   def first_non_repeating_letter(string)
     hash = Hash.new(0)
     0.upto(string.length - 1) do |i|
-      hash[string[i]] += 1
+      hash[string[i].downcase] += 1
     end
-    hash.select{|letter, frequency| frequency == 1}.first[0]
+    one_occurrence_letters = hash.select{|letter, frequency| frequency == 1}
+    return "" if one_occurrence_letters.empty?
+    index = one_occurrence_letters.first[0]
+    return "" if index.nil?
+    string[index]
   end
   
 
