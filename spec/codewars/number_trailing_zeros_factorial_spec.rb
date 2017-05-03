a5ef7002af647499ff9d4a4983b3fdc5e5324d8e
Fatos Morina <fatosi.morina@gmail.com>
2017-05-03 21:13:55 +0200

    Add spec testing number of trailing zeros for 1000! factorial

--- spec/codewars/number_trailing_zeros_factorial_spec.rb
+++ spec/codewars/number_trailing_zeros_factorial_spec.rb
@@ -2,9 +2,15 @@ require '../../lib/codewars/number_trailing_zeros_factorial'
 
 describe NumberOfTrailingZerosOfFactorial do 
 
-	it 'should return zero for 5! = 125' do 
+	it 'should return 1 for 5! = 125' do 
 		expect(subject.zeros(5)).to eq(1)
 	end
+
+	it 'should return 249 for 1000!' do 
+		expect(subject.zeros(1000)).to eq(249)
+	end
+
+	
 
 
 end
