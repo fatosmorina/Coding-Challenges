5b5132a15c4b3669390570f7312572c76d3b1219
Fatos Morina <fatosi.morina@gmail.com>
2017-05-04 12:32:26 +0200

    Add a new solution to PrimeTime

--- lib/codewars/prime_time.rb
+++ lib/codewars/prime_time.rb
@@ -10,4 +10,20 @@ class PrimeTime
 		primes.to_a
 	end
 
+
+	#Alternative solution
+	def alternative_prime(n)
+	  	primes = generate_primes(n)
+	end
+
+
+	private 
+
+	def generate_primes(number)
+		primes = [nil, nil, *2..number]
+		2.upto(Math.sqrt(number)) do |i|
+			(i**2..n).step(i){|j| primes[j] = nil if primes[j]}
+		end
+		primes
+	end
 end
