919d398ff8fcb9a642cadd48c0266a46e7620e04
Fatos Morina <fatosi.morina@gmail.com>
2017-05-04 11:33:38 +0200

    Test PrimeTime with n = 11

--- spec/codewars/prime_time_spec.rb
+++ spec/codewars/prime_time_spec.rb
@@ -5,4 +5,8 @@ describe PrimeTime do
 	it 'should return [2,3] for n = 3' do 
 		expect(subject.prime(3)).to eq([2,3])
 	end 
+
+	it 'should return [2,3,5,7,11] for n = 12' do 
+		expect(subject.prime(11)).to eq([2,3,5,7,11])
+	end 
 end 
