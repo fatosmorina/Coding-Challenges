5b0ef100af7315641b0dafdb6b75cf93dd4ca83f
Fatos Morina <fatosi.morina@gmail.com>
2017-06-12 15:03:10 +0200

    Add spec description

--- spec/codewars/jaden_casing_string_spec.rb
+++ spec/codewars/jaden_casing_string_spec.rb
@@ -2,7 +2,7 @@ require '../../lib/codewars/jaden_casing_string'
 
 describe JadenCasingString do
 
-  it "should return" do 
+  it "should return camel cased sentence" do 
     str = "How can mirrors be real if our eyes aren't real"
     output = "How Can Mirrors Be Real If Our Eyes Aren't Real"
     expect(subject.solution(str)).to eq(output)
