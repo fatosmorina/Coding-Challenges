343285a9487803af3bfa97153267a0b4fd4f13a5
Fatos Morina <fatosi.morina@gmail.com>
2017-07-02 13:50:57 +0200

    Add more meaningful problem test case description at RelativeRank

--- spec/leetcode/relative_ranks_spec.rb
+++ spec/leetcode/relative_ranks_spec.rb
@@ -2,8 +2,8 @@ require '../../lib/leetcode/relative_ranks'
 
 describe RelativeRanks do
 
-  it 'should return custom array for array with 5 elements' do
+  it 'should return custom array for consequetive array elements' do
     expect(subject.find_relative_ranks([5,4,3,2,1])).to eq(["Gold Medal","Silver Medal","Bronze Medal","4","5"])
   end
-  
+
 end
