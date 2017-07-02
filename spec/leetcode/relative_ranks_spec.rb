d5f0705b97cf76199695786775f73c8c4a097bb8
Fatos Morina <fatosi.morina@gmail.com>
2017-07-02 13:52:06 +0200

    Return custom array with 3 elements

--- spec/leetcode/relative_ranks_spec.rb
+++ spec/leetcode/relative_ranks_spec.rb
@@ -11,5 +11,8 @@ describe RelativeRanks do
     expect(subject.find_relative_ranks([10,3,8,9,4])).to eq(["Gold Medal","5","Bronze Medal","Silver Medal","4"])
   end
 
+  it 'should return custom array for array with 3 elements' do
+    expect(subject.find_relative_ranks([1,3,2])).to eq(["Bronze Medal","Gold Medal","Silver Medal"])
+  end
   
 end
