f8d8d8b673cdff4d4acd5f234a747eb08f0c7300
Fatos Morina <fatosi.morina@gmail.com>
2017-07-20 19:32:10 +0200

    Add problem description at ConvertToCamelCaseString

--- lib/codewars/convert_to_camelcase.rb
+++ lib/codewars/convert_to_camelcase.rb
@@ -1,3 +1,17 @@
+=begin
+
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+
+Examples:
+
+# returns "theStealthWarrior"
+to_camel_case("the-stealth-warrior") 
+
+# returns "TheStealthWarrior"
+to_camel_case("The_Stealth_Warrior")
+
+=end
+
 #https://www.codewars.com/kata/517abf86da9663f1d2000003/train/ruby
 
 class ConvertToCamelCase
