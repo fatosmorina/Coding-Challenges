b4a2ea746fcf4468e5add6128e5120b68c76438c
Fatos Morina <fatosi.morina@gmail.com>
2017-07-23 21:55:46 +0200

    Add problem description at BitCounting

--- lib/codewars/bit_counting.rb
+++ lib/codewars/bit_counting.rb
@@ -1,3 +1,8 @@
+=begin
+Write a function that takes an (unsigned) integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+=end
 
 #https://www.codewars.com/kata/bit-counting/train/ruby
 
