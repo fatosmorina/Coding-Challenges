23f28b750410f40611837f3cfe25346bba04d651
Fatos Morina <fatosi.morina@gmail.com>
2017-07-23 21:52:56 +0200

    Test BitCounting with n = 4

--- spec/codewars/bit_counting_spec.rb
+++ spec/codewars/bit_counting_spec.rb
@@ -5,4 +5,9 @@ describe BitCounting do
   it 'should return 0 for n = 0' do
     expect(subject.count_bits(0)).to eq(0)
   end
+
+   it 'should return 1 for n = 4' do
+    expect(subject.count_bits(4)).to eq(1)
+  end
+
 end
