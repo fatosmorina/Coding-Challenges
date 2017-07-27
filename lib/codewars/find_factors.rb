8704d58e5a834626087c2b23c3a56a9c92390acc
Fatos Morina <fatosi.morina@gmail.com>
2017-07-27 19:32:10 +0200

    Add problem description at FindFactors

--- lib/codewars/find_factors.rb
+++ lib/codewars/find_factors.rb
@@ -1,7 +1,17 @@
+=begin
+
+In this Kata you have to find the factors of integer down to the limit including the limiting number. There will be no negative numbers. Return the result as an array of numbers in ascending order.
+
+If the limit is more than the integer, return an empty list
+
+As a challenge, see if you can do it in one line
+
+=end
+
 #https://www.codewars.com/kata/find-factors-down-to-limit/train/ruby
 
 class FindFactors
- 
+
   def factors(integer, limit)
     return [] if limit > integer
     result = [integer]
