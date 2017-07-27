c84fd9adf3aa3ff9a34b937e48eec706cf84cd1a
Fatos Morina <fatosi.morina@gmail.com>
2017-07-27 19:32:10 +0200

    Add problem description at dUPLICATEeNCODE

--- lib/codewars/extract_domain_name.rb
+++ lib/codewars/extract_domain_name.rb
@@ -1,3 +1,13 @@
+=begin
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+domain_name("http://github.com/carbonfive/raygun") == "github" 
+domain_name("http://www.zombie-bites.com") == "zombie-bites"
+domain_name("https://www.cnet.com") == "cnet"
+
+=end
+
 #https://www.codewars.com/kata/514a024011ea4fb54200004b/train/ruby
 
 class ExtractDomainName
