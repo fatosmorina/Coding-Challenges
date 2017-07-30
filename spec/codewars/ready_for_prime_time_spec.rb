cfdcfce91301ce48c533684cf73acb40ef564336
Fatos Morina <fatosi.morina@gmail.com>
2017-07-30 09:47:05 +0200

    Test ready prime time for n = 11

--- spec/codewars/ready_for_prime_time_spec.rb
+++ spec/codewars/ready_for_prime_time_spec.rb
@@ -4,4 +4,9 @@ describe ReadyForPrimeTime do
   it 'should return the array for n = 11' do
     expect(subject.first_solution(11)).to eq([2,3,5,7,11])
   end
+
+   it 'should return the array for n = 6' do
+    expect(subject.first_solution(6)).to eq([2,3,5])
+  end
+
 end
