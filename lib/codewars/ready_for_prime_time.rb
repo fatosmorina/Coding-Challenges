2e60c605f954bd6d2a71c2cba55d93eb5b5c3fce
Fatos Morina <fatosi.morina@gmail.com>
2017-07-30 09:50:10 +0200

    Add a second solution for ready prime time

--- lib/codewars/ready_for_prime_time.rb
+++ lib/codewars/ready_for_prime_time.rb
@@ -3,5 +3,22 @@ class ReadyForPrimeTime
     require 'prime'
     Prime.take_while{|p| p <= n}
   end
+
+  def second_solution(n)
+    primes = []
+
+    2.upto(n) do |i|
+      primes[i] = i
+    end
+
+    2.upto(Math.sqrt(n)) do |i|
+      next unless primes[i]
+      (i*i).step(n, i) do |j|
+        primes[j] = nil
+      end
+    end
+    
+    primes.compact  
+  end
   
 end
