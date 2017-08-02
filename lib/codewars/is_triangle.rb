d4adf210c128113b8ca3313cf7438d82b2340b18
Fatos Morina <fatosi.morina@gmail.com>
2017-08-02 19:32:10 +0200

    Add problem description at IsaTriangle

--- lib/codewars/is_triangle.rb
+++ lib/codewars/is_triangle.rb
@@ -1,3 +1,11 @@
+=begin
+
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+=end
+
 class IsTriangle
 
   def is_triangle(a, b, c)
