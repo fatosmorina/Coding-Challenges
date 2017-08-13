48fd6a893ea4708c85c228be8af295c10bcbb288
Fatos Morina <fatosi.morina@gmail.com>
2017-08-13 15:09:36 +0200

    Fix the issue with end

--- lib/codewars/unique_in_words.rb
+++ lib/codewars/unique_in_words.rb
@@ -1,17 +1,17 @@
 #https://www.codewars.com/kata/54e6533c92449cc251001667/train/ruby
+#
 
 class UniqueInWords
-  
-   def unique_in_order(iterable)
-   return [] if iterable.length == 0
-   end
-    result = [iterable[0]]
-    1.upto(iterable.length - 1) do |i|
-      if iterable[i-1] != iterable[i]
-        result<<iterable[i]
-      end
+
+  def unique_in_order(iterable)
+    return [] if iterable.length == 0
+  end
+  result = [iterable[0]]
+  1.upto(iterable.length - 1) do |i|
+    if iterable[i-1] != iterable[i]
+      result<<iterable[i]
     end
     result
   end
- 
+
 end
