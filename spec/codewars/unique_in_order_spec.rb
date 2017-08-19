91fd1e4e698f6b54f87e83929683a5c1d2643b73
Fatos Morina <fatosi.morina@gmail.com>
2017-08-19 14:55:43 +0200

    Solve NextPerfectSquare

--- spec/codewars/unique_in_order_spec.rb
+++ spec/codewars/unique_in_order_spec.rb
@@ -4,4 +4,9 @@ describe UniqueInOrder do
   it 'should get a list of unique characters' do
     expect(subject.unique_in_order('AAAB')).to eq(['A','B'])
   end
+
+   it 'should work for an empty string' do
+    expect(subject.unique_in_order('')).to eq([])
+  end
+
 end
