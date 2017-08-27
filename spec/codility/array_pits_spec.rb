aafb4d829bebf2cb60984536269f36adbce034d6
Fatos Morina <fatosi.morina@gmail.com>
2017-08-27 12:42:19 +0200

    Delete unurelated classes

--- spec/codility/array_pits_spec.rb
+++
@@ -1,8 +0,0 @@
-require '../../lib/codility/array_pits'
-describe ArrayPits do
-
-  it 'returns 4' do
-    expect(subject.solution([0,1,3,-2,0,1,0,-3,2,3])).to eq(4)
-  end
-
-end
--- spec/codility/linked_list_spec.rb
+++
@@ -1,9 +0,0 @@
-require '../../lib/codility/linked_list'
-describe LinkedList do
-
-  it 'should return 1' do
-    expect(subject.solution([])).to eq(1)
-  end
-
-
-end
