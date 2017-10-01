dbdc8c1132a46f7c4d2fca8f7e0a6f25cdf28ee3
Fatos Morina <fatosi.morina@gmail.com>
2017-10-01 12:05:41 +0200

    Test LeftRotationWith100Elements

--- spec/hacker_rank/data_structures/arrays/left_rotation_spec.rb
+++ spec/hacker_rank/data_structures/arrays/left_rotation_spec.rb
@@ -1,4 +1,5 @@
 require '../../../../lib/hacker_rank/data_structures/arrays/left_rotation/'
+require 'pry'
 
 describe LeftRotation do
   it 'should rotate the array with 5 elements' do
@@ -11,6 +12,11 @@ describe LeftRotation do
     expect(subject.left_rotation(array, 3)).to eq([])
   end
 
+  it 'should not rotate the array with 100 elements' do
+    array =*(1..100)
+    rotated_array = *(2..100)
+    rotated_array<<1
+    expect(subject.left_rotation(array, 1)).to eq(rotated_array)
+  end
 end
-
 
