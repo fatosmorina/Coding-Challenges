317cbdc9c295a0cc9f62f20a72788bea4cb03db6
Fatos Morina <fatosi.morina@gmail.com>
2017-11-20 10:56:24 +0100

    Implement a faster algorithm

--- lib/codility/perm_check.rb
+++ lib/codility/perm_check.rb
@@ -1,13 +1,19 @@
-#https://codility.com/demo/results/trainingHWYQK6-WHJ/
+#https://codility.com/demo/results/trainingHWYQK6-WHJ
+require 'set'
 class PermCheck
-
   def solution(array)
-    array.sort!
-    length = array.length
-    if (1..length).to_a == array
+    set = Set.new
+    max = array[0]
+    0.upto(array.length - 1) do |i|
+      return 0 if set.include?(array[i])
+      set << array[i]
+      max = array[i] if max < array[i]
+    end
+    if max == set.count
       1
     else
       0
     end
   end
 end
+
