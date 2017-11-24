9d187b84b2722dc590503a4e11a527f934caa04e
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 15:44:27 +0100

    Test hashtag_generator with a long string

--- spec/codewars/hashtag_generator_spec.rb
+++ spec/codewars/hashtag_generator_spec.rb
@@ -9,4 +9,9 @@ describe GenerateHashtag do
   it "should return the hashtag for the string = 'Hello there thanks for trying my Kata'" do 
     expect(subject.generateHashtag("Hello there thanks for trying my Kata")).to eq("#HelloThereThanksForTryingMyKata")
   end
+  
+  it "should return false for a very long string" do 
+    expect(subject.generateHashtag("hello world"*100)).to eq(false)
+  end
+
 end
