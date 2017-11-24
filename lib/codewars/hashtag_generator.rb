55aa0f6af43a6fdb98c6592a7dd98f04d16eecef
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 15:38:19 +0100

    Add class declaration at HashtagGenerator

--- lib/codewars/hashtag_generator.rb
+++ lib/codewars/hashtag_generator.rb
@@ -1,4 +1,6 @@
-def generateHashtag(n)
-  return false if n == "" || n.length > 140
-  "#".concat(n.split(" ").map(&:capitalize).join(" ").gsub(" ",""))
+class GenerateHashtag
+  def generateHashtag(n)
+    return false if n == "" || n.length > 140
+    "#".concat(n.split(" ").map(&:capitalize).join(" ").gsub(" ",""))
+  end
 end
