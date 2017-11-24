723c448bc8be4882d2fc0924836c6ee90daaccd4
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 15:46:04 +0100

    Add new tests for generate_hashtag

--- spec/codewars/generate_hashtags_spec.rb
+++ spec/codewars/generate_hashtags_spec.rb
@@ -2,8 +2,16 @@ require '../../lib/codewars/generate_hashtags'
 
 describe GenerateHashTags do 
 
-	it 'should return #HelloWord for input Hello World' do 
-		expect(subject.generate_hashtags('Hello World')).to eq('#HelloWorld')
-	end
+  it "should return the hashtag for the string = 'hello world'" do 
+    expect(subject.generate_hashtags("hello world")).to eq("#HelloWorld")
+  end
 
+  it "should return the hashtag for the string = 'Hello there thanks for trying my Kata'" do 
+    expect(subject.generate_hashtags("Hello there thanks for trying my Kata")).to eq("#HelloThereThanksForTryingMyKata")
+  end
+
+  it "should return false for a very long string" do 
+    expect(subject.generate_hashtags("hello world"*100)).to eq(false)
+  end
 end
+
