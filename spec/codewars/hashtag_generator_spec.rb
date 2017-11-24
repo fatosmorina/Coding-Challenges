ad1113eaee56b66eb7b9f883864709464853597c
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 15:41:35 +0100

    Test hashtag_generator with hello world string

--- spec/codewars/hashtag_generator_spec.rb
+++ spec/codewars/hashtag_generator_spec.rb
@@ -2,8 +2,10 @@ require '../../lib/codewars/hashtag_generator'
 
 describe GenerateHashtag do
 
-  it 'should' do 
+  it "should return the hashtag for the string = 'hello world'" do 
     expect(subject.generateHashtag("hello world")).to eq("#HelloWorld")
   end
+
+
 end
 
