07b2445b649467f4b7cde5f5de01d73357856d0f
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 17:43:36 +0100

    Solve CompletePattern

--- lib/codewars/complete_pattern.rb
+++ lib/codewars/complete_pattern.rb
@@ -1,14 +1,16 @@
-def pattern(n)
-  return "" if n < 1
-  return "1" if n == 1
-  output = ""
-  j = 0
-  while j != n
+class CompletePattern
+  def pattern(n)
+   return "" if n < 1
+   return "1" if n == 1
+   output = ""
+   j = 0
+   while j != n
     n.downto(j+1) do |i|
-      output.concat("#{i}")
+       output.concat("#{i}")
     end
     output.concat("\n")
     j += 1
+   end
+   output[0, output.length - 1]
   end
-  output[0, output.length - 1]
 end
