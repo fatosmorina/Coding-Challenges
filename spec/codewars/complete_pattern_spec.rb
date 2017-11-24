59abac0ac77ccd1519ea291a47477ad9fb65aceb
Fatos Morina <fatosi.morina@gmail.com>
2017-11-24 17:48:42 +0100

    Add test spec for n = 6

--- spec/codewars/complete_pattern_spec.rb
+++ spec/codewars/complete_pattern_spec.rb
@@ -5,5 +5,10 @@ describe CompletePattern do
   it "should return the pattern for n = 3" do
     expect(subject.pattern(3)).to eq("321\n32\n3")
   end
+
+  it "should return the pattern for n = 6" do
+    expect(subject.pattern(6)).to eq("654321\n65432\n6543\n654\n65\n6")
+  end
+
 end
 
