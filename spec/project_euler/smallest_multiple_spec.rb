f74381dcc663f53ec872c94869cdc7cd27e10045
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:34:26 +0100

    Test SmallestMultiple for n = 20

--- spec/project_euler/smallest_multiple_spec.rb
+++ spec/project_euler/smallest_multiple_spec.rb
@@ -8,4 +8,12 @@ describe SmallestMultiple do
   it 'should return 2520 for n = 10' do
     expect(subject.solution(20)).to eq(232792560)
   end
+
+  it 'should return 2520 for n = 10 for faster_solution' do
+    expect(subject.faster_solution(10)).to eq(2520)
+  end
+
+  it 'should return 232792560 for n = 20 for faster_solution' do
+    expect(subject.faster_solution(20)).to eq(232792560)
+  end
 end
