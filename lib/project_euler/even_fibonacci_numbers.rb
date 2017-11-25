a7253ffe02420bc63b408eb1bb30bec6a87207b1
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 18:46:55 +0100

    Solve EvenFibonacciNumbers

---
+++ lib/project_euler/even_fibonacci_numbers.rb
@@ -0,0 +1,14 @@
+  def solution(number)
+    array = [1,1]
+    sum = 0
+    current_number = 1 + 1
+    while (current_number) <= number
+      array.push(current_number)
+      if current_number.even?
+        sum += current_number
+      end
+      current_number = array.last + array[array.length - 2]
+    end
+    sum
+  end
+
