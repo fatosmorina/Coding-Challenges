8101200f7504fb99376f9840fea13cdd293387a1
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 18:59:55 +0100

    Test EvenFibonacciNumbers for n = 4000000

--- spec/project_euler/even_fibonacci_numbers_spec.rb
+++ spec/project_euler/even_fibonacci_numbers_spec.rb
@@ -2,8 +2,11 @@ require '../../lib/project_euler/even_fibonacci_numbers'
 
 describe EvenFibonacciNumbers do
 
-  it 'should return 231 for n = 10' do
+  it 'should return 10 for n = 10' do
     expect(subject.solution(10)).to eq(10)
   end
-end
 
+  it 'should return 4613732 for n = 4000000' do
+    expect(subject.solution(4000000)).to eq(4613732)
+  end
+end
