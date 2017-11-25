4b0c27645358af7dc5f14a0f77a65f05aaa7568f
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 15:55:55 +0100

    Test LargestPrimeFactor for n = 600851475143

--- spec/project_euler/largest_prime_factor.rb
+++ spec/project_euler/largest_prime_factor.rb
@@ -5,5 +5,8 @@ describe LargestPrimeFactor do
   it 'should return 29 for n = 13195' do
     expect(subject.solution(13195)).to eq(29)
   end
-end
 
+  it 'should return 6857 for n = 600851475143' do
+    expect(subject.solution(600851475143)).to eq(6857)
+  end
+end
