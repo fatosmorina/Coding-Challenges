8a646f294535580d1f38fa56ac905d5944e5463f
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:02:01 +0100

    Test LargestPalindromeProduct for n = 3

--- spec/project_euler/largest_palindrome_product_spec.rb
+++ spec/project_euler/largest_palindrome_product_spec.rb
@@ -4,4 +4,8 @@ describe LargestPalindromeProduct do
   it 'should return 9009 for n = 2' do
     expect(subject.solution(2)).to eq(9009)
   end
+  
+  it 'should return 906609 for n = 3' do
+    expect(subject.solution(3)).to eq(906609)
+  end
 end
