e7f9312aad2f39d66bce8801ce679507b3c193e5
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 19:12:55 +0100

    Test MultiplesOf3And5 for n = 1000

--- spec/project_euler/multiples_of_3_and_5_spec.rb
+++ spec/project_euler/multiples_of_3_and_5_spec.rb
@@ -5,4 +5,8 @@ describe MultiplesOf3And5 do
   it 'should return 23 for n = 10' do
     expect(subject.solution(10)).to eq(23)
   end
+
+  it 'should return 233168 for n = 1000' do
+    expect(subject.solution(1000)).to eq(233168)
+  end
 end
