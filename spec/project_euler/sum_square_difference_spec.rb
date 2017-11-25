d7877353ada4abcdc6e906191fa34b1d433c0507
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:43:28 +0100

    Test SumSquareDifference for n = 100

--- spec/project_euler/sum_square_difference_spec.rb
+++ spec/project_euler/sum_square_difference_spec.rb
@@ -4,4 +4,9 @@ describe SumSquareDifference do
   it 'should return 2640 for n = 10' do
     expect(subject.solution(10)).to eq(2640)
   end
+
+  it 'should return 25164150 for n = 100' do
+    expect(subject.solution(100)).to eq(25164150)
+  end
+
 end
