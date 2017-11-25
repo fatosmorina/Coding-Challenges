7d7efa34fca88832ca0c986a34f99f4b03b3bb3a
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:01:12 +0100

    Test LargestPalindromeProduct for n = 2

---
+++ spec/project_euler/largest_palindrome_product_spec.rb
@@ -0,0 +1,7 @@
+require '../../lib/project_euler/largest_palindrome_product'
+
+describe LargestPalindromeProduct do
+  it 'should return 9009 for n = 2' do
+    expect(subject.solution(2)).to eq(9009)
+  end
+end
