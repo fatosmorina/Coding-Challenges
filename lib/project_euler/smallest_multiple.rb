5c1df456af71a01419aa3b2984f946710eefab6d
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:15:32 +0100

    Solve SmallestMultiple

--- lib/project_euler/smallest_multiple.rb
+++ lib/project_euler/smallest_multiple.rb
@@ -1,14 +1,16 @@
-def solution(upper_range)
-  (1..upper_range)
-  n = upper_range
-  loop do
-    divisors = []
-    (2..upper_range).to_a.each do |a|
-      if n%a == 0
-        divisors << a
+class SmallestMultiple
+  def solution(upper_range)
+    n = upper_range
+    loop do
+      divisors = []
+      (2..upper_range).to_a.each do |a|
+        if n%a == 0
+          divisors << a
+        end
       end
+      return n if divisors.length == (upper_range-1)
+      n += 1
     end
-    return n if divisors.length == (upper_range-1)
-    n += 1
   end
 end
+
