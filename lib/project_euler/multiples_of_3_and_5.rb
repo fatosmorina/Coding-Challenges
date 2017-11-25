64b6046b4974d8838532ae9b2ef2a9609fc587ac
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 19:08:50 +0100

    Solve MultiplesOf3And5

---
+++ lib/project_euler/multiples_of_3_and_5.rb
@@ -0,0 +1,15 @@
+class MultiplesOf3And5
+ def solution(number)
+  multiples = []
+  (number-1).downto(3) do |i|
+    if i%3 == 0 && i%5 == 0
+      multiples << i
+    elsif i%5 == 0
+      multiples << i
+    elsif i%3 == 0
+      multiples << i
+    end
+  end
+  multiples.inject(:+)
+ end
+end
