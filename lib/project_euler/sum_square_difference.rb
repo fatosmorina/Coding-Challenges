99e321276640c0418bb0f80e64e24b58639b1830
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 20:40:15 +0100

    Solve SumSquareDifference

--- lib/project_euler/sum_square_difference.rb
+++ lib/project_euler/sum_square_difference.rb
@@ -1,6 +1,8 @@
-def solution(n)
-  sum_squares = 0
-  (1..n).to_a.each{|i| sum_squares += i**2}
-  square_sum = (n*(n+1)/2)**2
-  square_sum - sum_squares
+class SumSquareDifference
+  def solution(n)
+    sum_squares = 0
+    (1..n).to_a.each{|i| sum_squares += i**2}
+    square_sum = (n*(n+1)/2)**2
+    square_sum - sum_squares
+  end
 end
