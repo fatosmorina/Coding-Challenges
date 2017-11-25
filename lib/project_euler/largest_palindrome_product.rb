71cee332bd38672039a86b7c86eee58c930caa0f
Fatos Morina <fatosi.morina@gmail.com>
2017-11-25 19:58:15 +0100

    Solve LargestPalindromeProduct

--- lib/project_euler/largest_palindrome_product.rb
+++ lib/project_euler/largest_palindrome_product.rb
@@ -1,14 +1,16 @@
-def solution(number_of_digits)
-  palindromes = []
-  (10**(number_of_digits)-1).downto(10**(number_of_digits-1)+1) do |i|
+class LargestPalindromeProduct
+  def solution(number_of_digits)
+   palindromes = []
+   (10**(number_of_digits)-1).downto(10**(number_of_digits-1)+1) do |i|
     i.downto(10**(number_of_digits-1)+1) do |j|
       palindromes<< i*j if is_palindrome?(i*j)
     end
+   end
+   palindromes
   end
-  palindromes
-end
 
-def is_palindrome?(n)
-  n.to_s == n.to_s.reverse
+private
+  def is_palindrome?(n)
+    n.to_s == n.to_s.reverse
+  end
 end
-
