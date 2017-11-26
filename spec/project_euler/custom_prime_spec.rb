80424886cb21c7d4218b8bb1195d70742e2f6d0f
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 08:33:41 +0100

    Test CustomPrime for n = 10001

--- spec/project_euler/custom_prime_spec.rb
+++ spec/project_euler/custom_prime_spec.rb
@@ -4,4 +4,8 @@ describe CustomPrime do
   it 'should return 13 for n = 6' do
     expect(subject.solution(6)).to eq(13)
   end
+
+  it 'should return 13 for n = 10001' do
+    expect(subject.solution(10001)).to eq(104743)
+  end
 end
