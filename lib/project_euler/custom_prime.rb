7c87a312a37e8e51e18c1540921cfe18e18d8297
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 07:43:15 +0100

    Add problem description at CustomPrime

--- lib/project_euler/custom_prime.rb
+++ lib/project_euler/custom_prime.rb
@@ -1,7 +1,15 @@
-#!/usr/bin/env ruby
+=begin
 
-# What is the 10001st prime number
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+=end
+
+#https://projecteuler.net/problem=7
+
 require 'prime'
+
 class CustomPrime
   def faster_solution(number)
     count = 0
@@ -30,3 +38,4 @@ class CustomPrime
     true
   end
 end
+
