9e92b09eda75e5f767170b2d5d1810c580762856
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 11:14:07 +0100

    Add problem description  at SummationOfPrimes

--- lib/project_euler/summation_of_primes.rb
+++ lib/project_euler/summation_of_primes.rb
@@ -1,4 +1,10 @@
+=begin
 
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+=end
 #https://projecteuler.net/problem=10
 
 class SummationOfPrimes
