b7e2440c8e80d309830d830054be6eace2759ed8
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 08:53:12 +0100

    Refactor CustomPrime

--- lib/project_euler/custom_prime.rb
+++ lib/project_euler/custom_prime.rb
@@ -1,5 +1,6 @@
 =begin
 
+
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10 001st prime number?
@@ -10,27 +11,35 @@ What is the 10 001st prime number?
 
 class CustomPrime
   def faster_solution(number)
+    solve(number, "fast")
+  end
+
+  def solution(number)
+    solve(number, "slow")
+  end
+
+private
+
+  def solve(number, prime_testing_type)
     require 'prime'
     count = 1
     i = 2
     until count == number
       i += 1
+      count = count_prime(prime_testing_type, count, i)
+    end
+    i
+  end
+
+  def count_prime(prime_testing_type, count, i)
+    if prime_testing_type == "fast"
+      count += 1 if is_prime?(i)
+    elsif prime_testing_type == "slow"
       count += 1 if Prime.prime?(i)
     end
-    i
+    count
   end
 
-  def solution(number)
-    count = 1
-    i = 2
-    until count == number
-      i += 1
-      count += 1 if is_prime?(i)
-    end
-    i
-  end
-
-private
   def is_prime?(number)
     2.upto(number**0.5) do |i|
       return false if number%i == 0
