4eafd976ce08d506580573fe4bfedd3d3b0aef94
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 08:34:51 +0100

    Test CustomPrime for n = 13 when using Prime module

--- spec/project_euler/custom_prime_spec.rb
+++ spec/project_euler/custom_prime_spec.rb
@@ -8,4 +8,9 @@ describe CustomPrime do
   it 'should return 13 for n = 10001' do
     expect(subject.solution(10001)).to eq(104743)
   end
+
+  it 'should return 13 for n = 6 when using Prime module' do
+    expect(subject.faster_solution(6)).to eq(13)
+  end
+
 end
