8e113ea7a4090d23d589a32fdd47a85f0a64d5f2
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 11:36:14 +0100

    Test PowerDigitSum for n = 2**1000|

--- spec/project_euler/power_digit_sum_spec.rb
+++ spec/project_euler/power_digit_sum_spec.rb
@@ -4,4 +4,8 @@ describe PowerDigitSum do
   it 'should return 32768 for n = 2**15' do
     expect(subject.solution(2, 15)).to eq(26)
   end
+
+  it 'should return 32768 for n = 2**1000' do
+    expect(subject.solution(2, 1000)).to eq(1366)
+  end
 end
