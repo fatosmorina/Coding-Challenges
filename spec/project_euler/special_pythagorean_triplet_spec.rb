aff9849220ad0de8a7a10489e5cfa07a8798c8ae
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 09:36:23 +0100

    Test SpecialPythagoreanTriplet

---
+++ spec/project_euler/special_pythagorean_triplet_spec.rb
@@ -0,0 +1,7 @@
+require '../../lib/project_euler/special_pythagoeran_triplet'
+
+describe SpecialPythagoreanTriplet do
+  it 'should return the product = 31875000' do
+    expect(subject.solution).to eq(31875000)
+  end
+end
