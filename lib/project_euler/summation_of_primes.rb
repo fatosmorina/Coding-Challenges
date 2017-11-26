82a919e53dec2e6451c14b748a8f2505d4e4f281
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 11:13:10 +0100

    Solve SummationOfPrimes

--- lib/project_euler/summation_of_primes.rb
+++ lib/project_euler/summation_of_primes.rb
@@ -1,16 +1,17 @@
-def solution(n)
-  primes = get_primes(n)
-  primes.inject(:+)
-end
-
-
-def get_primes(n)
-  primes = (0..n).to_a
-  primes[0] = primes[1] = nil
-  primes.each do |prime|
-    next if prime.nil?
-    break if prime*prime > n
-    (prime*prime).step(n, prime) {|a| primes[a] = nil}
+class SummationOfPrimes
+  def solution(n)
+    primes = get_primes(n)
+    primes.inject(:+)
   end
-  primes.compact
+
+  def get_primes(n)
+    primes = (0..n).to_a
+    primes[0] = primes[1] = nil
+    primes.each do |prime|
+      next if prime.nil?
+      break if prime*prime > n
+      (prime*prime).step(n, prime) {|a| primes[a] = nil}
+    end
+    primes.compact
+  end
 end
