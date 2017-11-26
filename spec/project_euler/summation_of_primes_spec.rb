2870fa64b557a4bee706ffa19ffdb950722dbc5e
Fatos Morina <fatosi.morina@gmail.com>
2017-11-26 11:16:48 +0100

    Test SummationOfPrimes for n = 2000000

--- spec/project_euler/summation_of_primes_spec.rb
+++ spec/project_euler/summation_of_primes_spec.rb
@@ -3,4 +3,7 @@ describe SummationOfPrimes do
   it 'should return 17 for n = 10' do
     expect(subject.solution(10)).to eq(17)
   end
+    it 'should return 142913828922 for n = 2000000' do
+    expect(subject.solution(2000000)).to eq(142913828922)
+  end
 end
